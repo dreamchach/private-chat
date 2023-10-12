80fd5345eea16180618d2c47966ea273b4c36f24
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-10-12 12:55:03 +0900

    배포중...

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:4000', {autoConnect : false})
+const socket = io('https://port-0-private-chat-koh2xlj3ufoqd.sel4.cloudtype.app', {autoConnect : false})
 
 socket.onAny((event, ...args) => {
     console.log(event, ...args)
