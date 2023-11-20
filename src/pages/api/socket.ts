b34c604674393bbb33c0a315d4714f93d3264770
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-20 15:41:11 +0900

    socket connected ...

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,3 +1,31 @@
+
+import { Server } from "socket.io"
+
+export default function SocketHandler(req : any, res : any) {
+  if(res.socket.server.io) {
+    console.log('now binding...')
+  }else {
+    const io = new Server(res.socket.server)
+
+    io.use((socket, next) => {
+      console.log('socket middleware')
+
+      next()
+    })
+
+    io.on('connection', async (socket) => {
+      console.log('socket connect')
+
+      socket.on('disconnect', () => {
+        console.log('disconnect')
+      })
+    })
+    res.socket.server.io = io
+  }
+  res.end()
+}
+
+/*
 import dbConnect from "@/lib/dbConnect"
 import MessageModel from "@/model/Message"
 import { getToken } from "@/utill/functions/api"
@@ -78,3 +106,4 @@ export default function SocketHandler(req : any , res : any) {
   }
   res.end()
 }
+*/
