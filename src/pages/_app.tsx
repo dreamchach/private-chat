5bf9bb6c49817db4a3c88f913009ea5aa916c908
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-20 14:32:24 +0900

    socket connected

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,10 @@ import type { AppProps } from 'next/app'
 import { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { io } from 'socket.io-client'
+import { io, Socket } from 'socket.io-client';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [socket, setSocket] = useState<any>()
+  const [socket, setSocket] = useState<Socket | null>(null);
   useEffect(() => {
     const socketConnect = async () => {
       await axios.get('/api/socket')
