07944e872cb2f3406f0bae84a22dd95ef9184d49
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-24 18:43:20 +0900

    create socket api

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,7 +1,28 @@
 import connect from "@/lib/dbConnect"
+import { Server } from "socket.io"
 
 export default function handler(req: any, res: any) {
   connect()
-  
-  res.status(200).json({ name: 'John Doe' })
+
+    if(res.socket.server.io) {
+    console.log('now binding...')
+  }else {
+    const io = new Server(res.socket.server)
+
+    io.use((socket, next) => {
+      console.log('socket middleware')
+
+      next()
+    })
+
+    io.on('connection', async (socket) => {
+      console.log('socket connect')
+
+      socket.on('disconnect', () => {
+        console.log('disconnect')
+      })
+    })
+    res.socket.server.io = io
+  }
+  res.end()
 }
