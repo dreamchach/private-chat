b61b2d095d1d97d369e5b386228bad8fb8d9b097
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-25 21:52:53 +0900

    notice icon

--- src/components/friends/AboutFriends.tsx
+++ src/components/friends/AboutFriends.tsx
@@ -3,7 +3,7 @@ import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import FriendAvatar from './FriendAvatar';
 
 const AboutFriends = ({friend, wait} : any) => {
-    console.log('component', wait)
+    console.log('component', wait.waitPeople.includes(friend.userId), 'friend', friend)
     return (
         <div  className='mx-14 flex items-center justify-between gap-5 mt-5 hover:bg-none-button p-2 rounded-xl'>
             <FriendAvatar friend={friend}/>
