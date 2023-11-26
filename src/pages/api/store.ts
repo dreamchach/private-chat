76ce0169e326d595fe69af386c70f9ce041e5d9a
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-26 14:44:14 +0900

    /api/store

--- src/pages/api/store.ts
+++ src/pages/api/store.ts
@@ -24,7 +24,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         } else {
           const data = {
             userToken : token,
-            messages : [payload]
+            messages : []
           }
           const message = new messageModel(data)
           const saveMessage = message.save()
