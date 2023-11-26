75173e62777cd1c565dc1476f63e87a412293fe0
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-26 15:10:28 +0900

    first message input error

--- src/pages/api/store.ts
+++ src/pages/api/store.ts
@@ -24,7 +24,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         } else {
           const data = {
             userToken : token,
-            messages : []
+            messages : [payload]
           }
           const message = new messageModel(data)
           const saveMessage = message.save()
