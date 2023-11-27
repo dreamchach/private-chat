ad08fb4d0e4f773970cb44bcd264192fad28f565
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-27 11:18:33 +0900

    layout designing...

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 import React from 'react'
 import { FaUser } from "react-icons/fa";
 import { IoChatbubble } from "react-icons/io5";
 
 const Layout = () => {
+  const router = useRouter()
+  console.log(router)
   return (
     <div className='h-screen fixed bg-slate-100'>
-        <Link href='/friends'>
-          <div className='w-14 text-4xl my-16 text-slate-400 hover:text-slate-950'>
+        <Link href='/friends' className='my-16'>
+          <div className='w-14 text-4xl text-slate-400 hover:text-slate-950'>
             <FaUser />
           </div>
-          <div className='text-slate-400 hover:text-slate-950 text-xs'>친구</div>
+          <div className='flex items-center justify-center text-slate-400 hover:text-slate-950 text-xs'>친구</div>
         </Link>
-        <Link href='/rooms'>
-          <div className='w-14 text-4xl my-16 text-slate-400 hover:text-slate-950'>
+        <Link href='/rooms' className='my-16'>
+          <div className='w-14 text-4xl text-slate-400 hover:text-slate-950'>
             <IoChatbubble />
           </div>
-          <div className='text-slate-400 hover:text-slate-950 text-xs'>채팅</div>
+          <div className='flex items-center justify-center text-slate-400 hover:text-slate-950 text-xs'>채팅</div>
         </Link>
     </div>
   )
