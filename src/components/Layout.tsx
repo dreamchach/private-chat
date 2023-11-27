f4076673c78e5a59ace27ebba06b9a96b43c21d6
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-27 11:13:23 +0900

    layout designing...

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,18 @@ import { IoChatbubble } from "react-icons/io5";
 
 const Layout = () => {
   return (
-    <div>
+    <div className='h-screen fixed bg-slate-100'>
         <Link href='/friends'>
-          <FaUser />
+          <div className='w-14 text-4xl my-16 text-slate-400 hover:text-slate-950'>
+            <FaUser />
+          </div>
+          <div className='text-slate-400 hover:text-slate-950 text-xs'>친구</div>
         </Link>
         <Link href='/rooms'>
-          <IoChatbubble />
+          <div className='w-14 text-4xl my-16 text-slate-400 hover:text-slate-950'>
+            <IoChatbubble />
+          </div>
+          <div className='text-slate-400 hover:text-slate-950 text-xs'>채팅</div>
         </Link>
     </div>
   )
