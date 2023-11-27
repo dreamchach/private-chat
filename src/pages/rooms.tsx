d76037576d55cabb3b4b3b094242b11d02057301
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-27 20:58:38 +0900

    rooms socket

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,11 +1,55 @@
 import Layout from '@/components/Layout'
 import FriendsHeader from '@/components/friends/FriendsHeader'
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { FaPlus } from "react-icons/fa";
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { io } from 'socket.io-client';
 
 const rooms = () => {
   const dispatch = useDispatch()
+  const router = useRouter()
+  const auth = useSelector((state : any) => {
+    return state.auth
+  })
+
+  let socket : any
+  
+  const socketInitializer = async () => {
+    await axios.get('/api/socket')
+    socket = io()
+    socket.auth = auth
+
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+/*
+    socket.on('users-data', ({users} : any) => {
+      const notMe = users.filter((user : any) => {
+        const answer = user.userId !== auth.userId
+        return answer
+      })
+      setFriends(notMe)
+    })
+*/
+
+    socket.on('error', (error : any) => {
+      console.log('error')
+    })
+  }
+  
+  useEffect(() => {
+    if(auth.userId !== '' && auth.nickName !== '') {
+      socketInitializer()
+    }else router.push('/')
+  
+    return () => {
+      if(socket) {
+        socket.disconnect()
+      }
+    }
+  }, [])
 
   return (
     <div className='flex flex-row'>
