93366a71ad22b156c9ec9e91ffc1ee64361dd3d3
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-27 20:49:32 +0900

    /rooms page designing

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,11 +1,23 @@
 import Layout from '@/components/Layout'
+import FriendsHeader from '@/components/friends/FriendsHeader'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { FaPlus } from "react-icons/fa";
 
 const rooms = () => {
+  const dispatch = useDispatch()
+
   return (
     <div className='flex flex-row'>
         <Layout />
-        <div>rooms</div>
+        <div className='ml-14 w-full'>
+          <FriendsHeader dispatch={dispatch} />
+          <div>
+            <button>
+              <FaPlus /> 채팅방 생성
+            </button>
+          </div>
+        </div>
     </div>
   )
 }
