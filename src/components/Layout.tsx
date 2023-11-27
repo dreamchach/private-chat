6002ea25504fe74c32e389aa0f05398db0f7c9e3
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-11-27 10:30:21 +0900

    layout add icons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
+import { FaUser } from "react-icons/fa";
+import { IoChatbubble } from "react-icons/io5";
 
 const Layout = () => {
   return (
     <div>
-        <Link href='/friends'>friends</Link>
-        <Link href='/rooms'>rooms</Link>
+        <Link href='/friends'>
+          <FaUser />
+        </Link>
+        <Link href='/rooms'>
+          <IoChatbubble />
+        </Link>
     </div>
   )
 }
